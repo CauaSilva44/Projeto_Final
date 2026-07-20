aee8c0c35e5cc11b9b81ca6e4c68275f832a150d
CauaSilva44 <cauadasilvadias54@gmail.com>
2026-07-20 10:09:15 -0300

    Main

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,7 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
 
